e99a5d6712276091011231aaeb98e140d9ea7f7e
nailazaa <naylaizati22@gmail.com>
2025-01-22 14:29:48 +0700

    auth clear

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 });
 
 Route::resource('/posts', App\Http\Controllers\PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
